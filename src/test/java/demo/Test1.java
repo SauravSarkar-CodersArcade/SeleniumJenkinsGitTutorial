11b490952dc86c2fb0160db9d9a6e03aa032cf0c
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2022-10-20 21:24:37 +0530

    First Local Commit : Added The Project

---
+++ src/test/java/demo/Test1.java
@@ -0,0 +1,42 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Test1 {
+
+    private WebDriver driver;
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", projectPath + "/driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("http://www.saucedemo.com/");
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        Thread.sleep(2000);
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }
+}
+
